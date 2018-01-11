cb70809b5d7912afee7076d034cdfda2868096db
Arnelle Balane <arnellebalane@gmail.com>
2018-01-12 06:24:12 +0800

    Disable sequelize database query logging

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const databaseConfig = require('../config/database');
 const config = require('../config');
 
 const env = config.get('NODE_ENV');
-const defaultdb = new Sequelize(databaseConfig[env].url);
-const turndb = new Sequelize(config.get('TURN_DATABASE_URL'));
+const defaultdb = new Sequelize(databaseConfig[env].url, { logging: false });
+const turndb = new Sequelize(config.get('TURN_DATABASE_URL'), { logging: false });
 
 module.exports = { defaultdb, turndb };
