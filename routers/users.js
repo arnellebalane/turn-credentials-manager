6f8fde93c18a493fecf28e6c1a9994a022b0c1ca
Arnelle Balane <arnellebalane@gmail.com>
2018-01-12 07:37:49 +0800

    Check if email or username is already in use

--- routers/users.js
+++ routers/users.js
@@ -2,14 +2,20 @@ const pick = require('lodash/pick');
 const { post } = require('server/router');
 const { status, json } = require('server/reply');
 const { User } = require('../database/models');
+const Op = User.sequelize.Op;
 
 module.exports = [
     post('/users', async ctx => {
         const data = pick(ctx.data, ['username', 'email']);
-        const user = await User.findOne({ where: data });
-        if (user) {
-            return status(400);
-        }
+        const user = await User.findOne({
+            where: {
+                [Op.or]: [
+                    { username: data.username },
+                    { email: data.email }
+                ]
+            }
+        });
+        if (user) return status(400);
 
         const result = await User.create(data);
         return json(result.get());
