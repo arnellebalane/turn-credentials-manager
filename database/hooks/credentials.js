592b7bec3a007fc53a016d6cb81fdc77a9f413a0
Arnelle Balane <arnellebalane@gmail.com>
2018-01-13 18:51:53 +0800

    Add hook to delete TurnUser and TurnSecret when no more valid Credentials

--- database/hooks/credentials.js
+++ database/hooks/credentials.js
@@ -1,3 +1,5 @@
+const { Op } = require('sequelize');
+
 function createTurnUser({ Credential, TurnUser }) {
     Credential.afterCreate('createTurnUser', async (credential, options) => {
         const user = await credential.getUser();
@@ -7,11 +9,35 @@ function createTurnUser({ Credential, TurnUser }) {
         };
         const turnUser = await TurnUser.findOne({ where: turnUserData });
         if (!turnUser) {
-            TurnUser.create(turnUserData);
+            await TurnUser.create(turnUserData);
         }
+    });
+}
+
+function deleteTurnUserAndTurnSecret({ Credential, TurnUser, TurnSecret }) {
+    Credential.afterDestroy('deleteTurnUserAndTurnSecret', async (credential, options) => {
+        const user = await credential.getUser();
+        const credentials = await user.getCredentials({
+            where: {
+                expiresOn: {
+                    [Op.gt]: Date.now()
+                }
+            }
+        });
+        if (credentials.length > 0) return undefined;
+
+        await Promise.all([
+            TurnUser.destroy({
+                where: { name: user.username }
+            }),
+            TurnSecret.destroy({
+                where: { realm: credential.realm }
+            })
+        ]);
     });
 }
 
 module.exports = (models) => {
     createTurnUser(models);
+    deleteTurnUserAndTurnSecret(models);
 };
